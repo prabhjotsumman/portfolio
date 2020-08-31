2e8049cca4ec87d881218638e56170bcc5c1038e
prabhnith <prabhjot.nith@gmail.com>
2020-08-31 22:11:32 +0530

    Background.js updated

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,17 +1,44 @@
 var CONSTANTS = {
-    LABEL :{
-        I_AM: 'I\'m',
-        NAME: 'Prabhjot S',
-        EMAIL: 'prabhjot.nith@gmail.com',
-        GREETINGS_EN: 'Hello!',
-        GREETINGS_FR: 'Bonjour!',
-        GET_IN_TOUCH: 'Get in touch!',
-        TITLE: 'who is design-minded and focused on building user-friendly, responsive and beautiful interfaces & experiences',
-        CONTENT_ITEM_1 : 'React Developer',
-        CONTENT_ITEM_2 : 'Frontend Developer',
-        CONTENT_ITEM_3 : 'UI Developer',
-        CONTENT_ITEM_4 : 'Web Developer',
-    }
-}
+  LABEL: {
+    I_AM: "I'm",
+    NAME: "Prabhjot ",
+    EMAIL: "prabhjot.nith@gmail.com",
+    GREETINGS_EN: "Hello!",
+    GREETINGS_FR: "Bonjour!",
+    GET_IN_TOUCH: "Get in touch!",
+    TITLE:
+      "who is design-minded and focused on building user-friendly, responsive and beautiful interfaces & experiences",
+    CONTENT_ITEM_1: "React Developer",
+    CONTENT_ITEM_2: "Frontend Developer",
+    CONTENT_ITEM_3: "UI Developer",
+    CONTENT_ITEM_4: "Web Developer",
+    WHEN_I_AM_NOT_WORKING: "When I'm not coding, ",
+    WHEN_I_AM_NOT_WORKING_DESC:
+      "When I'm not coding, I will most likely be playing video games, XBOX, PS4, football, fooseball, working-out or watching Netflix.",
+  },
+  FILLERS: {
+    IN: "in ",
+  },
+  BACKGROUND: {
+    BACKGROUND: "Background",
+    CURRENTLY_WORKING_AS: "I'm currently working as a Frontend Developer at ",
+    CURRENT_COMPANY: "PUBLICIS SAPIENT ",
+    CURRENT_COMPANY_URL: "https://www.publicissapient.com/",
+    BACKGROUND_DESC_1:
+      "creating products and services for the web with some awesome people. ",
+    BACKGROUND_DESC_2:
+      "As a Developer, I truly enjoy bridging the gap between design and engineering — combining my technical knowledge with my keen eye for design to build a beautiful . I derive great satisfaction from building better, more efficient code as well as working in details and designs of the product to achieve superb UX/UI.",
+    WORKED_BEFORE: "",
+    WORKED_BEFORE_COMPANY: "",
+    WORKED_BEFORE_COMPANY_URL: "",
+    GRADUATED: "I have graduated from ",
+    COLLEGE:
+      "National Institute of Technology Hamirpur, Himachal Pradesh (NIT HAMIRPUR) ",
+    COLLEGE_DESC:
+      "It is one of the premier colleges in India (Tier I/ Tier II).",
+    GRADUATED_YEAR: "2017 ",
+    GRADUATED_BRANCH: "Computer Science and Engineering. ",
+  },
+};
 
 export default CONSTANTS;
--- src/components/Background.js
+++ src/components/Background.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
 import TrackVisibility from "react-on-screen";
+import CQ from "../Constants/Constants";
 
 import "../css/background/background.css";
 
@@ -24,47 +25,28 @@ function Background() {
       <div className="Background">
         <div className="outterContainer">
           <animated.div style={topProps}>
-            <h1 className="Bg-Header">BACKGROUND</h1>
+            <h1 className="Bg-Header">BACKGROUND!</h1>
           </animated.div>
           <animated.div style={bottomProps} className="Bg-Container">
             <p>
-              {" "}
-              I'm currently working as a Frontend Developer at{" "}
-              <a href="https://onikon.com/" target="_blank">
-                ONIKON Creative Inc.
+              {CQ.BACKGROUND.CURRENTLY_WORKING_AS}
+              <a href={CQ.BACKGROUND.CURRENT_COMPANY_URL}>
+                {CQ.BACKGROUND.CURRENT_COMPANY}
               </a>{" "}
-              creating products and services for the web with some awesome
-              people. I am a formal Product Engineer graduated from{" "}
-              <a href="https://www.ubc.ca/" target="_blank">
-                UBC
-              </a>{" "}
-              and recently attained a Web Development Diploma from{" "}
-              <a href="https://www.lighthouselabs.ca/" target="_blank">
-                Lighthouse Labs.
-              </a>{" "}
-              I have also worked as a Web Developer Intern at{" "}
-              <a href="https://www.kater.com/" target="_blank">
-                Kater Technologies Inc.
-              </a>
+              {CQ.BACKGROUND.BACKGROUND_DESC_1}
             </p>
-
             <br />
-
             <p>
-              As a Developer, I truly enjoy bridging the gap between design and
-              engineering — combining my technical knowledge with my keen eye
-              for design to build a beautiful product. I derive great
-              satisfaction from building better, more efficient code as well as
-              working in details and designs of the product to achieve superb
-              UX/UI.
+              {CQ.BACKGROUND.GRADUATED} {CQ.BACKGROUND.COLLEGE} {CQ.FILLERS.IN}
+              {CQ.BACKGROUND.GRADUATED_YEAR} {CQ.FILLERS.IN}{" "}
+              {CQ.BACKGROUND.GRADUATED_BRANCH} {CQ.BACKGROUND.COLLEGE_DESC}
             </p>
-
             <br />
-
+            <p>{CQ.BACKGROUND.BACKGROUND_DESC_2}</p>
+            <br />
             <p>
-              <span>When I'm not coding,</span> I will most likely be playing
-              video games, basketball, snowboarding, working-out or watching
-              Netflix.
+              <span>{CQ.LABEL.WHEN_I_AM_NOT_WORKING}</span>{" "}
+              {CQ.LABEL.WHEN_I_AM_NOT_WORKING_DESC}
             </p>
           </animated.div>
         </div>
